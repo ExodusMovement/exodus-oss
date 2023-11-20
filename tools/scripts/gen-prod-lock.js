756ef2c7a224366be13d68b4f8179981250e1e34
Mark Vayngrib <mvayngrib@gmail.com>
2023-11-20 12:45:32 -0500

    chore: add gen-prod-lock script

---
+++ tools/scripts/gen-prod-lock.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const { execSync } = require('child_process')
+
+const cwd = process.cwd()
+const OUTPUT_PATH = path.resolve(cwd, 'yarn.lock.prod')
+
+const run = (cmd) => execSync(cmd, { encoding: 'utf8' })
+
+const pkgJsonPaths = run(`find . -name package.json -type f | grep -v /node_modules/`)
+  .trim()
+  .split('\n')
+  .map((filePath) => filePath.trim())
+  .filter(Boolean)
+  .map((filePath) => path.resolve(cwd, filePath))
+
+for (const pkgJsonPath of pkgJsonPaths) {
+  const pkgJson = require(pkgJsonPath)
+  if (!pkgJson.devDependencies) continue
+
+  console.log(`found devDependencies in: ${pkgJsonPath}`)
+  delete pkgJson.devDependencies
+  fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2))
+}
+
+run(`yarn install && \
+  echo "yarn.lock now contains only prod deps. After you're done with it, don't forget to kill all unstaged changes."`)
