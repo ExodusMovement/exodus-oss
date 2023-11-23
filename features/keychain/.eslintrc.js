b05cb6db5996d1d9eb9bd1de51684c7ba170909c
Mark Vayngrib <mvayngrib@gmail.com>
2023-11-23 12:25:19 -0500

    chore: add types

--- features/keychain/.eslintrc.js
+++ features/keychain/.eslintrc.js
@@ -1,6 +1,7 @@
 const path = require('path')
 module.exports = {
   extends: '../../.eslintrc.js',
+  tsconfigRootDir: '../../',
   parserOptions: {
     babelOptions: {
       configFile: path.join(path.dirname(__filename), 'babel.config.js'),
---
+++ features/keychain/module/index.d.ts
@@ -0,0 +1,2 @@
+export { createKeyIdentifierForExodus, EXODUS_KEY_IDS, KeyIdentifier } from './key-identifier'
+export { default, Keychain } from './keychain'
