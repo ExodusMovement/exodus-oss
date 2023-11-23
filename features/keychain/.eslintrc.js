6c8a55929fd12fd988ea0c510bb732a5816ff127
Mark Vayngrib <mvayngrib@gmail.com>
2023-11-23 14:38:04 -0500

    lint

--- features/keychain/.eslintrc.js
+++ features/keychain/.eslintrc.js
@@ -2,6 +2,7 @@ const path = require('path')
 module.exports = {
   extends: '../../.eslintrc.js',
   parserOptions: {
+    tsconfigRootDir: '../../',
     babelOptions: {
       configFile: path.join(path.dirname(__filename), 'babel.config.js'),
     },
