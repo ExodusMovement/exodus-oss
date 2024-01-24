377b6a75679924484cba53b3f94bb3230ae696e3
Jan W <jan.we@exodus.io>
2024-01-24 21:10:39 +0800

    chore: fix linting setup (#24)

--- features/keychain/.eslintrc.js
+++ features/keychain/.eslintrc.js
@@ -7,4 +7,12 @@ module.exports = {
       configFile: path.join(path.dirname(__filename), 'babel.config.js'),
     },
   },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.js'],
+      rules: {
+        'import/no-unresolved': 'off', // TODO: add required deps once open-sourced and remove this
+      },
+    },
+  ],
 }
