92f4e889dfef792de54a7cbf52e93fe737c89c41
Mark Vayngrib <mvayngrib@gmail.com>
2024-02-06 16:35:17 -0500

    feat!: export multiSeedKeychain definition instead of keychain

--- features/keychain/index.js
+++ features/keychain/index.js
@@ -1,16 +1,9 @@
-import memoizedKeychainDefinition from './module/memoized-keychain'
-import keychainDefinition from './module/keychain'
 import multiSeedKeychainDefinition from './module/multi-seed-keychain'
 
 const keychain = ({ cachePublicKeys }) => {
   return {
     id: 'keychain',
-    definitions: [
-      multiSeedKeychainDefinition,
-      ...(cachePublicKeys
-        ? [{ definition: memoizedKeychainDefinition, storage: { namespace: 'keychain' } }]
-        : [{ definition: keychainDefinition }]),
-    ],
+    definitions: [multiSeedKeychainDefinition],
   }
 }
 
