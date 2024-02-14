38322ea29788b47b4d5c5040326547ff634ffec6
Mark Vayngrib <mvayngrib@gmail.com>
2024-02-14 12:22:38 -0500

    test: pass seedId explicitly in ecdsa tests

--- features/keychain/module/__tests__/ecdsa.test.js
+++ features/keychain/module/__tests__/ecdsa.test.js
@@ -2,14 +2,13 @@ import { mnemonicToSeed } from 'bip39'
 
 import { createKeyIdentifierForExodus } from '@exodus/key-ids'
 import createKeychain from './create-keychain'
-import keychainDefinition from '../multi-seed-keychain'
 import { getSeedId } from '../crypto/seed-id'
-
-const { factory: createMultiSeedKeychain } = keychainDefinition
 
 const seed = mnemonicToSeed(
   'menu memory fury language physical wonder dog valid smart edge decrease worth'
 )
+
+const seedId = getSeedId(seed)
 
 const secondSeed = mnemonicToSeed(
   'wine system mean beyond filter human meat rubber episode wash stomach aunt'
@@ -23,7 +22,7 @@ describe('EcDSA Signer', () => {
 
     const signer = keychain.createSecp256k1Signer(fusionKeyId)
     const plaintext = Buffer.from('I really love keychains')
-    const signature = await signer.signBuffer({ data: plaintext })
+    const signature = await signer.signBuffer({ seedId, data: plaintext })
     const expected =
       '3045022100e3c37a346dcb717552e9591a43b3f099898cb0a7d2c5aa37447fb0146926bbec022057a2e393efbd154f55278844f402e05c5bedd6a6c205c4b293e50c83813e67a5'
     expect(signature.toString('hex')).toBe(expected)
@@ -32,7 +31,12 @@ describe('EcDSA Signer', () => {
   it('should signBuffer (using secp256k1 instance)', async () => {
     const keychain = createKeychain({ seed })
     const plaintext = Buffer.from('I really love keychains')
-    const signature = await keychain.secp256k1.signBuffer({ keyId: fusionKeyId, data: plaintext })
+    const signature = await keychain.secp256k1.signBuffer({
+      seedId,
+      keyId: fusionKeyId,
+      data: plaintext,
+    })
+
     const expected =
       '3045022100e3c37a346dcb717552e9591a43b3f099898cb0a7d2c5aa37447fb0146926bbec022057a2e393efbd154f55278844f402e05c5bedd6a6c205c4b293e50c83813e67a5'
     expect(signature.toString('hex')).toBe(expected)
@@ -44,17 +48,16 @@ describe.each([
   {
     primarySeed: seed,
     secondarySeed: secondSeed,
-    seedId: getSeedId(seed),
+    seedId,
   },
   {
     primarySeed: secondSeed,
     secondarySeed: seed,
-    seedId: getSeedId(seed),
+    seedId,
   },
 ])('EcDSA Signer (multi-seed-keychain)', ({ primarySeed, secondarySeed, seedId }) => {
   it('should signBuffer (using secp256k1 instance)', async () => {
-    const keychain = createMultiSeedKeychain()
-    keychain.addSeed(primarySeed)
+    const keychain = createKeychain({ seed: primarySeed })
     keychain.addSeed(secondarySeed)
 
     const plaintext = Buffer.from('I really love keychains')
