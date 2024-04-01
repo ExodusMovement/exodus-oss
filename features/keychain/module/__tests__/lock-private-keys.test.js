d8014357e739bbad6f58c6bc28110004053ab938
andrejborstnik <andrej-borstnik@hotmail.com>
2024-04-01 12:53:08 +0200

    feat: add tests for locked keychain

---
+++ features/keychain/module/__tests__/lock-private-keys.test.js
@@ -0,0 +1,114 @@
+import { mnemonicToSeed } from 'bip39'
+import { createKeyIdentifierForExodus } from '@exodus/key-ids'
+import KeyIdentifier from '@exodus/key-identifier'
+import createKeychain from './create-keychain'
+import { getSeedId } from '../crypto/seed-id'
+
+const seed = mnemonicToSeed(
+  'menu memory fury language physical wonder dog valid smart edge decrease worth'
+)
+
+const seedId = getSeedId(seed)
+
+describe('lockPrivateKeys', () => {
+  it('should allow private key usage when unlocked', async () => {
+    const keychain = createKeychain({ seed })
+    const keyId = createKeyIdentifierForExodus({ exoType: 'FUSION' })
+    const exportedKeys = await keychain.exportKey({
+      seedId,
+      keyId,
+      exportPrivate: true,
+    })
+
+    // Public keys should be the same
+    const sodiumEncryptor = keychain.createSodiumEncryptor(keyId)
+    const {
+      sign: { publicKey },
+    } = await sodiumEncryptor.getSodiumKeysFromSeed({ seedId })
+    expect(Buffer.compare(publicKey, exportedKeys.publicKey)).toBe(0)
+  })
+
+  it('should allow addSeed when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    keychain.addSeed(seed)
+  })
+
+  it('should allow addSeeds when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    keychain.addSeeds([seed])
+  })
+
+  it('should allow removeAllSeeds when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    keychain.removeAllSeeds()
+  })
+
+  it('should allow exportKey for public keys when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    const keyId = createKeyIdentifierForExodus({ exoType: 'FUSION' })
+    const exportedKeys = await keychain.exportKey({
+      seedId,
+      keyId,
+    })
+
+    expect(!!exportedKeys.publicKey).toBe(true)
+  })
+
+  it('should block exportKey for private keys when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    const keyId = createKeyIdentifierForExodus({ exoType: 'FUSION' })
+    await expect(
+      keychain.exportKey({
+        seedId,
+        keyId,
+        exportPrivate: true,
+      })
+    ).rejects.toThrow(/private keys are not locked/)
+  })
+
+  it('should block signTx when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    await expect(keychain.signTx({})).rejects.toThrow(/private keys are not locked/)
+  })
+
+  it('should block clone when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    await expect(async () => keychain.clone()).rejects.toThrow(/private keys are not locked/)
+  })
+
+  it('should block sodium when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    await expect(keychain.sodium.getSodiumKeysFromSeed({})).rejects.toThrow(
+      /private keys are not locked/
+    )
+    await expect(keychain.createSodiumEncryptor({}).getSodiumKeysFromSeed({})).rejects.toThrow(
+      /private keys are not locked/
+    )
+  })
+
+  it('should block ed25519 when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    await expect(keychain.ed25519.signBuffer({})).rejects.toThrow(/private keys are not locked/)
+    await expect(keychain.createEd25519Signer({}).signBuffer({})).rejects.toThrow(
+      /private keys are not locked/
+    )
+  })
+
+  it('should block secp256k1 when locked', async () => {
+    const keychain = createKeychain({ seed })
+    keychain.lockPrivateKeys()
+    await expect(keychain.secp256k1.signBuffer({})).rejects.toThrow(/private keys are not locked/)
+    await expect(keychain.createSecp256k1Signer({}).signBuffer({})).rejects.toThrow(
+      /private keys are not locked/
+    )
+  })
+})
