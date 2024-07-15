459ebe6a0685e15b491529f011098a4aa59a4864
Jan W <jan.we@exodus.io>
2024-07-15 16:19:52 +0200

    test: fix

--- libraries/key-identifier/__tests__/key-identifier.test.js
+++ libraries/key-identifier/__tests__/key-identifier.test.js
@@ -89,13 +89,12 @@ describe('KeyIdentifier', () => {
 
       const derived = keyId.derive([1, 5])
 
-      expect(derived).toEqual({
+      expect(derived.toJSON()).toEqual({
         derivationAlgorithm: 'BIP32',
         assetName: 'ethereum',
+        derivationPath: "m/44'/60'/0'/1/5",
         keyType: 'secp256k1',
       })
-
-      expect(derived.derivationPath).toBe("m/44'/60'/0'/1/5")
     })
   })
 
