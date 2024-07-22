a582ff7c27a6844d4eac3bd78b463b4fa9b16831
Marcos Kichel <marcos.kichel@gmail.com>
2024-07-22 20:53:56 -0300

    refactor: add getManySeedIds helper

--- features/keychain/module/crypto/seed-id.js
+++ features/keychain/module/crypto/seed-id.js
@@ -1,3 +1,5 @@
 import { fromMasterSeed } from '@exodus/bip32'
 
 export const getSeedId = (seed) => fromMasterSeed(seed).identifier.toString('hex')
+
+export const getManySeedIds = (seeds) => new Set(seeds.map(getSeedId))
