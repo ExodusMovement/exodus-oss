8a34220df0ebb39c18ab79da595c139c27e3676e
Marcos Kichel <marcos.kichel@gmail.com>
2024-07-23 13:08:07 -0300

    refactor: return array from getManySeedIds

--- features/keychain/module/crypto/seed-id.js
+++ features/keychain/module/crypto/seed-id.js
@@ -2,4 +2,4 @@ import { fromMasterSeed } from '@exodus/bip32'
 
 export const getSeedId = (seed) => fromMasterSeed(seed).identifier.toString('hex')
 
-export const getManySeedIds = (seeds) => new Set(seeds.map(getSeedId))
+export const getManySeedIds = (seeds) => seeds.map(getSeedId)
