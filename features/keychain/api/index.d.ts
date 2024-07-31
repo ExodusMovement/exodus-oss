efe2130e55b829d7dbf257dd068eb3e6bdc8ac0d
Marcos Kichel <marcos.ki@exodus.io>
2024-07-31 13:07:14 -0300

    refactor(keychain): add missing type definitions for keychain module

--- features/keychain/api/index.d.ts
+++ features/keychain/api/index.d.ts
@@ -4,6 +4,17 @@ import BN from 'bn.js'
 type SeedId = string
 type KeySource = { seedId: SeedId; keyId: KeyIdentifier }
 
+type Tx = Record<string, unknown>
+type HdKeys = { privateKey: Buffer; publicKey: Buffer }
+
+type SignTxCallback = (params: {
+  unsignedTx: Tx
+  hdkeys: HdKeys
+  privateKey: Buffer
+}) => void | Promise<void>
+
+type TxSignParams = KeySource & { signTxCallback: SignTxCallback; unsignedTx: Tx }
+
 type EcOptions = {
   canonical?: boolean
 }
@@ -24,16 +35,67 @@ type PrivateKeys = {
   xpriv: string
 }
 
+type PubKeyWrapper = {
+  publicKey: Buffer
+}
+
+type SignerInput = { seedId: string; keyId: string; data: Buffer }
+
+type Encryptor = {
+  getSodiumKeysFromSeed: (
+    args: KeySource
+  ) => Promise<{ box: PubKeyWrapper; sign: PubKeyWrapper; secret: Buffer }>
+  sign: (args: SignerInput) => Promise<Buffer>
+  signOpen: (args: SignerInput) => Promise<Buffer>
+  signDetached: (args: SignerInput) => Promise<Buffer>
+  verifyDetached: (
+    args: SignerInput & {
+      signature: Buffer
+    }
+  ) => Promise<boolean>
+  encryptSecretBox: (args: SignerInput) => Promise<Buffer>
+  decryptSecretBox: (args: SignerInput) => Promise<Buffer>
+  encryptBox: (args: SignerInput & { toPublicKey: Buffer }) => Promise<Buffer>
+  decryptBox: (
+    args: SignerInput & {
+      fromPublicKey: Buffer
+    }
+  ) => Promise<Buffer>
+  encryptSealedBox: (args: { data: Buffer; toPublicKey: Buffer }) => Promise<Buffer>
+  decryptSealedBox: (args: SignerInput) => Promise<Buffer>
+}
+
+type KeyId = Record<string, string>
+
+type Ed25519Signer = {
+  signBuffer: (args: SignerInput) => Promise<Buffer>
+}
+
+type Secp256k1Signer = {
+  signBuffer: (args: SignerInput) => Promise<Buffer>
+  signSchnorr: (args: SignerInput & { tweak: Buffer; extraEntropy: Buffer }) => Promise<Buffer>
+}
+
+type CreateSignerArgs = {
+  keyId: KeyId
+}
+
 export interface KeychainApi {
+  syncTx(params: TxSignParams): Promise<Tx>
+  addSeed(seed: Buffer): Promise<SeedId>
+  removeAllSeeds(): void
+  clear(): void
+  clone(): KeychainApi
+  createSodiumEncryptor(keyId: string): Encryptor
+  createEd25519Signer(args: CreateSignerArgs): Ed25519Signer
+  createSecp256k1Signer(args: CreateSignerArgs): Secp256k1Signer
   exportKey(params: KeySource): Promise<PublicKeys>
   exportKey(params: { exportPrivate: false } & KeySource): Promise<PublicKeys>
   exportKey(params: { exportPrivate: true } & KeySource): Promise<PublicKeys & PrivateKeys>
   arePrivateKeysLocked(seeds: Buffer[]): boolean
   sodium: {
     signDetached(params: { data: Buffer } & KeySource): Promise<Buffer>
-    getKeysFromSeed(
-      params: KeySource
-    ): Promise<{ box: { publicKey: Buffer }; sign: { publicKey: Buffer } }>
+    getKeysFromSeed(params: KeySource): Promise<{ box: PubKeyWrapper; sign: PubKeyWrapper }>
   }
   ed25519: {
     signBuffer(params: { data: Buffer } & KeySource): Promise<Buffer>
