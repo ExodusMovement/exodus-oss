2da2b05fdcc382042d3084cf63eb7f2931699fa4
fboucquez <fboucquez@gmail.com>
2024-11-13 17:09:11 +0000

    fix: add signSchnorr to keychain api (#171)

--- features/keychain/api/index.js
+++ features/keychain/api/index.js
@@ -16,6 +16,7 @@ const createKeychainApi = ({ keychain }) => {
       },
       secp256k1: {
         signBuffer: keychain.secp256k1.signBuffer,
+        signSchnorr: keychain.secp256k1.signSchnorr,
       },
     },
   }
